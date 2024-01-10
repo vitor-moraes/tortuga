8d37c83434ea0eb89ab1b9b0baae2a41915a5dba
Vitor Oliveira Moraes <vitor.oliveira.moraes@gmail.com>
2024-01-10 08:23:44 -0300

    ✨ feat: turning application into an api

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module TortugaBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.api_only = true
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  root 'habits#index'
-  get '/habits', to: 'habits#index'
-  # get up: 'rails/health#show', as: :rails_health_check
-  resources :habits, only: %i[index show new edit update destroy]
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :habits, only: %i[index show create edit update destroy]
 end
